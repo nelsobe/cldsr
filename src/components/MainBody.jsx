3af63fa93c2ab635958cc5f422ceb0b90e181db1
Brent Nelson <nelson@ee.byu.edu>
2024-06-04 20:54:27 -0600

    Rendering of table now working.

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import "./MainBody.css";
 
 export default function MainBody(props) {
-  // Have to wait until data is actually loaded
+  // Wait until data is actually loaded
   if (!props.text0 || !props.text1 || !props.text2) return <h1>Incomplete</h1>;
 
-  console.log(props.text1);
-
+  var tmp0, tmp1, tmp2;
+  // Keep color and prepare text to render
   if (props.colors) {
-    var tmp0 = props.text0.trim().split("\n");
-    var tmp1 = props.text1.trim().split("\n");
-    var tmp2 = props.text2.trim().split("\n");
+    [tmp0, tmp1, tmp2] = [props.text0, props.text1, props.text2].map((s) =>
+      s.trim().split("\n")
+    );
   } else {
-    var tmp0 = props.text0
-      .trim()
-      .replace(/tone[12345]/g, "tone6")
-      .split("\n");
-    var tmp1 = props.text1
-      .trim()
-      .replaceAll(/tone[12345]/g, "tone6")
-      .split("\n");
-    var tmp2 = props.text2
-      .trim()
-      .replaceAll(/tone[12345]/g, "tone6")
-      .split("\n");
+    // Toss color and prepare text to render
+    [tmp0, tmp1, tmp2] = [props.text0, props.text1, props.text2].map((s) =>
+      s
+        .trim()
+        .replace(/tone[12345]/g, "tone6")
+        .split("\n")
+    );
   }
 
   if (
@@ -50,24 +45,51 @@ export default function MainBody(props) {
         {tmp0.map((_, idx) => {
           return (
             <tr key={idx}>
-              <td
-                key={"0"}
-                dangerouslySetInnerHTML={{
-                  __html: tmp0.length == tmp0.length ? tmp0[idx] : null,
-                }}
-              />
-              <td
-                key={"1"}
-                dangerouslySetInnerHTML={{
-                  __html: tmp1.length == tmp0.length ? tmp1[idx] : null,
-                }}
-              />
-              <td
-                key={"2"}
-                dangerouslySetInnerHTML={{
-                  __html: tmp2.length == tmp0.length ? tmp2[idx] : null,
-                }}
-              />
+              {idx == 0 ? (
+                <th
+                  key={"0"}
+                  dangerouslySetInnerHTML={{
+                    __html: tmp0.length == tmp0.length ? tmp0[idx] : null,
+                  }}
+                />
+              ) : (
+                <td
+                  key={"0"}
+                  dangerouslySetInnerHTML={{
+                    __html: tmp0.length == tmp0.length ? tmp0[idx] : null,
+                  }}
+                />
+              )}
+              {idx == 0 ? (
+                <th
+                  key={"1"}
+                  dangerouslySetInnerHTML={{
+                    __html: tmp1.length == tmp0.length ? tmp1[idx] : null,
+                  }}
+                />
+              ) : (
+                <td
+                  key={"1"}
+                  dangerouslySetInnerHTML={{
+                    __html: tmp1.length == tmp0.length ? tmp1[idx] : null,
+                  }}
+                />
+              )}
+              {idx == 0 ? (
+                <th
+                  key={"2"}
+                  dangerouslySetInnerHTML={{
+                    __html: tmp2.length == tmp0.length ? tmp2[idx] : null,
+                  }}
+                />
+              ) : (
+                <td
+                  key={"2"}
+                  dangerouslySetInnerHTML={{
+                    __html: tmp2.length == tmp0.length ? tmp2[idx] : null,
+                  }}
+                />
+              )}
             </tr>
           );
         })}
