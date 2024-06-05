e58eb5254c70cf4ca27a90591c5b7bb9ac5b134a
Brent Nelson <nelson@ee.byu.edu>
2024-06-04 22:24:09 -0600

    Got AppBar working the way I want.  Next, work on cookies.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,33 +3,68 @@ import allbooks from "./allbooks";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import ToolBar from "@mui/material/ToolBar";
-
+import Box from "@mui/material/Box";
+import da from "../assets/da.jpg";
+import question from "../assets/question.jpg";
+import se from "../assets/se.jpg";
+import shang from "../assets/shang.jpg";
+import shu from "../assets/shu.jpg";
+import xia from "../assets/xia.jpg";
+import xiao from "../assets/xiao.jpg";
+import yan from "../assets/yan.jpg";
+import "./Header.css";
 
 function Header(props) {
-    function bookName() {
-        return "abc";
-        const tmp = allbooks.filter((bk) => { 
-            return bookName.id === props.name;
-        });
-        console.log(tmp);
-        return tmp.book;
-    }
-    
-  
-    return (
-        <AppBar position="static">
-            <ToolBar>
-                <button id="jssel"  onClick={props.jssel}>Sel</button>
-                <button id="jslangs"  onClick={props.jslangs}>Langs</button>
-                <button id="jscolors"  onClick={props.jscolors}>Colors</button>
-                <button id="jssizbig"  onClick={props.jssiz}>SizUp</button>
-                <button id="jssizsmall"  onClick={props.jssiz}>SizDn</button>
-                <button id="jsnavinc" onClick={props.jsnav}>+</button>
-                <button id="jsnavdec" onClick={props.jsnav}>-</button>
-                <button id="jsabout"  onClick={props.jsabout}>About</button>
-            </ToolBar>
-        </AppBar>
-    );
+  function bookName() {
+    return "abc";
+    const tmp = allbooks.filter((bk) => {
+      return bookName.id === props.name;
+    });
+    console.log(tmp);
+    return tmp.book;
+  }
+
+  const imgStyle = {
+    height: 1.4 * props.siz,
+    margin: 0.0 * props.siz,
+  };
+
+  const appbarStyle = {
+    backgroundColor: "Black",
+    height: 3 * props.siz,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const boxStyle = {
+    width: 1.2 * props.siz,
+  };
+
+  return (
+    <AppBar style={appbarStyle}>
+      <ToolBar>
+        <img style={imgStyle} src={shu} onClick={props.jssel} />
+        <Box style={boxStyle} />
+        <img style={imgStyle} src={yan} onClick={props.jslangs} />
+        <Box style={boxStyle} />
+        <img style={imgStyle} src={se} onClick={props.jscolors} />
+        <Box style={boxStyle} />
+        <img style={imgStyle} src={da} onClick={props.jssiz} id="jssizbig" />
+        <img
+          style={imgStyle}
+          src={xiao}
+          onClick={props.jssiz}
+          id="jssizsmall"
+        />
+        <Box style={boxStyle} />
+        <img style={imgStyle} src={xia} onClick={props.jsnav} id="jsnavinc" />
+        <img style={imgStyle} src={shang} onClick={props.jsnav} id="jsnavdec" />
+        <Box style={boxStyle} />
+        <img style={imgStyle} src={question} onClick={props.jsabout} />
+      </ToolBar>
+    </AppBar>
+  );
 }
 
 export default Header;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Box from "@mui/material/Box";
 
 export default function Title(props) {
   const h1style = {
     textAlign: "center",
     fontSize: 1.4 * props.siz,
     margin: 0,
-    marginTop: 6,
   };
   const h2style = { ...h1style, fontSize: 1.2 * props.siz };
 
+  const boxStyle = { height: 3.4 * props.siz };
   return (
     <div>
+      <Box style={boxStyle} width="100%" />
       <h1 style={h1style}>Welcome to CLDSR - Chinese Scriptures</h1>
       <h2 style={h2style}>
         {props.loc.book}{" "}
