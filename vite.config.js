8d280dc17ac1bfc9be38e5ca0c7b0689526e222e
Brent Nelson <nelson@ee.byu.edu>
2024-06-09 09:35:51 -0600

    Set to base for www.et.byu.edu

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // For running at www.et.byu.edu
-  // base: "~nelson/cldsr",
+  base: "~nelson/cldsr",
   // For running at nelsobe.github.io/cldsr
-  base: "/cldsr",
+  // base: "/cldsr",
   plugins: [react()],
 })
